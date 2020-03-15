89a08724e78b1c3f241039d0429e852a54dbfa55
agngan <agnieszka.gano@gmail.com>
2020-03-15 19:32:23 +0100

    Added repositories.

---
+++ src/main/java/com/lottery/repositories/DrawTypeRepository.java
@@ -0,0 +1,7 @@
+package com.lottery.repositories;
+
+import com.lottery.entities.DrawType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DrawTypeRepository extends CrudRepository<DrawType, Long> {
+}
---
+++ src/main/java/com/lottery/repositories/PriceWeightRepository.java
@@ -0,0 +1,7 @@
+package com.lottery.repositories;
+
+import com.lottery.entities.PriceWeight;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PriceWeightRepository extends CrudRepository<PriceWeight, Long> {
+}
