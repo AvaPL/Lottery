a7e0ca3d7251371b79a9e2ee058abd6e668c43ec
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-03-28 17:23:39 +0100

    Added scrolling table.

--- frontend/src/pages/Lotteries.js
+++ frontend/src/pages/Lotteries.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Lotteries.css'
+import Button from "react-bootstrap/Button";
 
 class Lotteries extends Component {
     render() {
@@ -9,19 +10,93 @@ class Lotteries extends Component {
                     <span className="lotteries-bar">LOTTERIES</span>
                 </div>
                 <div className="scroll-table-header">
-                <div className="row">
-                    <div className="col-sm">
-                        <span className="column-header">type</span>
-                    </div>
-                    <div className="col-sm">
-                        <span className="column-header">next draw</span>
-                    </div>
-                    <div className="col-sm">
-                        <span className="column-header">price to win</span>
+                    <div className="row">
+                        <div className="col-sm">
+                            <span className="column-header">type</span>
+                        </div>
+                        <div className="col-sm">
+                            <span className="column-header">next&nbsp;draw</span>
+                        </div>
+                        <div className="col-sm">
+                            <span className="column-header">price&nbsp;to&nbsp;win</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+                <div className="scroll-table overflow-auto flex-grow-1">
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                    <div>Test</div>
+                </div>
+                <Button className="buy-a-coupon-button btn-primary m-3"><span className="buy-a-coupon-text">BUY&nbsp;A&nbsp;COUPON</span></Button>
             </div>
         );
     }
