8671f47ba39d4676419988c2e94e310a2dea1703
agngan <agnieszka.gano@gmail.com>
2020-03-28 14:02:39 +0100

    Added multiple pages.

---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MainPage.css';
+import NavBar from "../components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function MainPage() {
+    return (
+        <div className="MainPage">
+            <span>Hej</span>
+        </div>
+    );
+}
+
+export default MainPage;
