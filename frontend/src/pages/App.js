186cdfd1e48975265ffe02554c47b5811d1bb1f5
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-03-28 14:15:28 +0100

    Added routing.

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import NavBar from "../components/NavBar";
 import {Route} from "react-router";
 import MainPage from "./MainPage";
+import Lotteries from "./Lotteries";
+import MyCoupons from "./MyCoupons";
 
 function App() {
     return (
         <div className="App">
             <NavBar/>
-            <Route exact path="/" component={MainPage} />
+            <Route exact path="/" component={MainPage}/>
+            <Route exact path="/lotteries" component={Lotteries}/>
+            <Route exact path="/my-coupons" component={MyCoupons}/>
         </div>
     );
 }
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './MainPage.css';
-import NavBar from "../components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MainPage() {
-    return (
-        <div className="MainPage">
-            <span>Hej</span>
-        </div>
-    );
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div className="MainPage">
+                <h1>MainPage</h1>
+            </div>
+        );
+    }
 }
 
 export default MainPage;
