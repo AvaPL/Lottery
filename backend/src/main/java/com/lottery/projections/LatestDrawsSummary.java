76d79457d117c7daaac5466ec7f8d32ca63e218a
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-04-06 20:11:41 +0200

    Added parsing numbers from database.

--- backend/src/main/java/com/lottery/projections/LatestDrawsSummary.java
+++ backend/src/main/java/com/lottery/projections/LatestDrawsSummary.java
@@ -9,22 +9,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Immutable
 @Entity
 @Data
 @Subselect("select *\n" +
-        "from (\n" +
-        "         select ld.id, dt.name as type, ld.DRAW_TIME as draw_date, p.price_won as price_won, numbers\n" +
-        "         from LOTTERY_DRAW LD\n" +
-        "                  join DRAW_TYPE DT on ld.DRAW_TYPE_ID = dt.ID\n" +
-        "                  join (select ld.id, sum(p.price) as price_won\n" +
-        "                        from LOTTERY_DRAW LD\n" +
-        "                                 join PRICE P on LD.ID = P.LOTTERY_DRAW_ID\n" +
-        "                        group by ld.id) P on ld.ID = p.id\n" +
-        "         where ld.DRAW_TIME < SYSDATE\n" +
-        "         order by ld.DRAW_TIME DESC)\n" +
-        "where ROWNUM <= 10")
+           "from (\n" +
+           "         select ld.id, dt.name as type, ld.DRAW_TIME as draw_date, p.price_won as price_won, numbers\n" +
+           "         from LOTTERY_DRAW LD\n" +
+           "                  join DRAW_TYPE DT on ld.DRAW_TYPE_ID = dt.ID\n" +
+           "                  join (select ld.id, sum(p.price) as price_won\n" +
+           "                        from LOTTERY_DRAW LD\n" +
+           "                                 join PRICE P on LD.ID = P.LOTTERY_DRAW_ID\n" +
+           "                        group by ld.id) P on ld.ID = p.id\n" +
+           "         where ld.DRAW_TIME < SYSDATE\n" +
+           "         order by ld.DRAW_TIME DESC)\n" +
+           "where ROWNUM <= 10")
 public class LatestDrawsSummary {
     @Id
     @GeneratedValue
@@ -32,11 +34,14 @@ public class LatestDrawsSummary {
     private String type;
     private Timestamp drawDate;
     private float priceWon;
-    private int numbers;
+    private long numbers;
 
     @JsonGetter
-    public int[] getNumbers(){
-        //TODO: implement
-        return new int[]{1, 2, 3, 4, 5, 6};
+    public Integer[] getNumbers() {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 64; i++)
+            if ((numbers >> i & 1) == 1)
+                result.add(i + 1);
+        return result.toArray(new Integer[0]);
     }
 }
