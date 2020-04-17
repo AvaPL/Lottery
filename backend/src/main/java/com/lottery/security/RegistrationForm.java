6b30be0e2e63f67e5e5dd4717bc4d9ce79102029
agngan <ganowicz.agnieszka@gmail.com>
2020-04-17 17:08:25 +0200

    Added validation to RegistrationForm.

--- backend/src/main/java/com/lottery/security/RegistrationForm.java
+++ backend/src/main/java/com/lottery/security/RegistrationForm.java
@@ -2,18 +2,34 @@ package com.lottery.security;
 
 import com.lottery.entities.Account;
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.constraints.*;
 import java.sql.Timestamp;
 
 @Data
 public class RegistrationForm {
+    @NotNull
+    @Size(min=3, max=30, message = "Username must be between 3 and 30 characters")
     private String username;
+    @NotNull
+    @Size(min=8, message = "Password must be longer than 8 characters")
     private String password;
+    @NotNull
+    @Size(min=8, message = "Password must be longer than 8 characters")
     private String repeatPassword;
+    @NotNull
+    @Email(message = "Invalid email address")
     private String email;
+    @NotNull
+    @CreditCardNumber(message ="Invalid credit card number")
     private String creditCardNumber;
+    @NotNull
+    @Pattern(regexp="^\\d{2}/\\d{2}$", message = "Date should be formatted as MM/YY")
     private String creditCardExpirationDate;
+    @NotNull
+    @Pattern(regexp = "^\\d{3}$", message = "Invalid credit card CVV number")
     private String cvv;
 
     public Account toAccount(PasswordEncoder passwordEncoder) {
