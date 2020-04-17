645daed9a46954b8247b0ffba8cf1261567d7f5d
agngan <ganowicz.agnieszka@gmail.com>
2020-04-17 20:52:19 +0200

    Added database constraints to Price entity.

--- backend/src/main/java/com/lottery/entities/Price.java
+++ backend/src/main/java/com/lottery/entities/Price.java
@@ -3,15 +3,19 @@ package com.lottery.entities;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 public class Price {
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = "PRICE_GENERATOR", sequenceName = "PRICE_SEQUENCE", allocationSize = 1, initialValue = 1000)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRICE_GENERATOR")
     private Long id;
-    private int hitsCount;
-    private float price;
+    @NotNull
+    private Integer hitsCount;
+    private Float price;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "lottery_draw_id")
     private LotteryDraw lotteryDraw;
