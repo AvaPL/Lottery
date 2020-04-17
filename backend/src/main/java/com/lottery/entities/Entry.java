5a541d5acabbf7a0007c801b785795f271693a79
agngan <ganowicz.agnieszka@gmail.com>
2020-04-17 20:42:57 +0200

    Added database constraints to Entry entity.

--- backend/src/main/java/com/lottery/entities/Entry.java
+++ backend/src/main/java/com/lottery/entities/Entry.java
@@ -3,18 +3,23 @@ package com.lottery.entities;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 public class Entry {
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = "ENTRY_GENERATOR", sequenceName = "ENTRY_SEQUENCE", allocationSize = 1, initialValue = 1000)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ENTRY_GENERATOR")
     private Long id;
-    private long numbers;
+    @NotNull
+    private Long numbers;
     private Float priceWon;
+    @NotNull
     @ManyToOne //TODO: Might be not optimal (eager fetch).
     @JoinColumn(name = "coupon_id")
     private Coupon coupon;
+    @NotNull
     @ManyToOne //TODO: Might be not optimal (eager fetch).
     @JoinColumn(name = "lottery_draw_id")
     private LotteryDraw lotteryDraw;
