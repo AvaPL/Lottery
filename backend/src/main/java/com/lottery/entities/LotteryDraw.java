a4613ca8a2f444575957affe42c110ff92350345
agngan <ganowicz.agnieszka@gmail.com>
2020-04-17 20:47:06 +0200

    Added database constraints LotteryDraw entity.

--- backend/src/main/java/com/lottery/entities/LotteryDraw.java
+++ backend/src/main/java/com/lottery/entities/LotteryDraw.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -12,14 +13,17 @@ import java.util.List;
 @Data
 public class LotteryDraw {
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = "LOTTERY_DRAW_GENERATOR", sequenceName = "LOTTERY_DRAW_SEQUENCE", allocationSize = 1, initialValue = 1000)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LOTTERY_DRAW_GENERATOR")
     private Long id;
+    @NotNull
     private Timestamp drawTime;
-    private long numbers;
+    private Long numbers;
     @OneToMany(mappedBy = "lotteryDraw")
     private List<Entry> entries;
     @OneToMany(mappedBy = "lotteryDraw")
     private List<Price> prices;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "draw_type_id")
     private DrawType drawType;
