9bee1c0dbcfdc9e4457086db44028556e63dafe0
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-04-22 17:11:47 +0200

    Added doc comments to procedures.

--- backend/src/main/resources/procedures/random_coupons.sql
+++ backend/src/main/resources/procedures/random_coupons.sql
@@ -4,20 +4,28 @@ as
     r_bet_time timestamp;
     r_account_id number(19);
 begin
+    -- Pick a seed for RNG
     seed := TO_CHAR(SYSTIMESTAMP, 'YYYYDDMMHH24MISSFFFF');
     DBMS_RANDOM.SEED(seed);
+
+    -- Insert coupons populated with random data
     for i in 1 ..range
         loop
+            -- Pick a random bet time in last 100 days
             r_bet_time := SYSDATE - DBMS_RANDOM.value(0, 100);
+
+            -- Assign coupon to random account
             select id
             into r_account_id
             from (select id
                   from ACCOUNT
                   order by DBMS_RANDOM.VALUE())
             where rownum = 1;
+
             insert into COUPON
             values (COUPON_SEQUENCE.nextval, r_bet_time, r_account_id);
         end loop;
+
     commit;
 end;
 /
--- backend/src/main/resources/procedures/random_entries.sql
+++ backend/src/main/resources/procedures/random_entries.sql
@@ -7,35 +7,47 @@ as
     r_numbers_count   int;
     r_max_value       int;
 begin
+    -- Pick a seed for RNG
     seed := TO_CHAR(SYSTIMESTAMP, 'YYYYDDMMHH24MISSFFFF');
     DBMS_RANDOM.SEED(seed);
+
     for i in 1 ..range
         loop
+            -- Pick a random coupon
             select id
             into r_coupon_id
             from (select id
                   from COUPON
                   order by DBMS_RANDOM.VALUE())
             where rownum = 1;
+
+            -- Pick a random lottery
             select id
             into r_lottery_draw_id
             from (select id
                   from LOTTERY_DRAW
                   order by DBMS_RANDOM.VALUE())
             where rownum = 1;
+
+            -- Pick the numbers count for the lottery draw type
             select dt.NUMBERS_COUNT
             into r_numbers_count
             from DRAW_TYPE dt
                      join LOTTERY_DRAW ld on dt.ID = ld.DRAW_TYPE_ID
             where ld.ID = r_lottery_draw_id;
+
+            -- Pick the max value for the lottery draw type
             select dt.MAX_VALUE
             into r_max_value
             from DRAW_TYPE dt
                      join LOTTERY_DRAW ld on dt.ID = ld.DRAW_TYPE_ID
             where ld.ID = r_lottery_draw_id;
+
             r_numbers := RANDOM_NUMBERS(r_numbers_count, r_max_value);
+
             insert into ENTRY VALUES (ENTRY_SEQUENCE.nextval, r_numbers, null, r_coupon_id, r_lottery_draw_id);
         end loop;
+
     commit;
 end;
 /
