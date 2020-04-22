e9728ac7bcacd439cc8c2c54f4ff38870e19653f
agngan <ganowicz.agnieszka@gmail.com>
2020-04-22 16:45:09 +0200

    Finished perform_lottery_draw procedure.

--- backend/src/main/resources/procedures/perform_lottery_draw.sql
+++ backend/src/main/resources/procedures/perform_lottery_draw.sql
@@ -1,9 +1,12 @@
 create or replace procedure perform_lottery_draw(p_lottery_draw_id in number)
 as
-    p_draw_type_id     number(19);
-    r_numbers          number(19);
-    numbers_count      int;
+    bitand_result     number(19);
     max_value          int;
+    numbers_count      int;
+    p_draw_type_id     number(19);
+    p_hits_count       int := 0;
+    p_price_won        float;
+    r_numbers          number(19);
     total_price        float;
     total_price_weight int;
 begin
@@ -28,7 +31,6 @@ begin
     set NUMBERS = r_numbers
     where ID = p_lottery_draw_id;
 
-    -- TODO: price.price
     select count(*) * max(dt.ENTRY_COST)
     into total_price
     from ENTRY e
@@ -50,7 +52,34 @@ begin
             set PRICE = TRUNC(p_price.WEIGHT / total_price_weight * total_price, 2)
             where PRICE.ID = p_price.ID;
         end loop;
-    -- TODO: entry.price_won
+
+    for p_entry in (select ID, NUMBERS from ENTRY where LOTTERY_DRAW_ID = 2)
+        loop
+            bitand_result := BITAND(r_numbers, p_entry.NUMBERS);
+            p_hits_count := 0;
+            while bitand_result > 0
+                loop
+                    bitand_result := BITAND(bitand_result, bitand_result - 1);
+                    p_hits_count := p_hits_count + 1;
+                end loop;
+
+            select (select PRICE
+                    from PRICE
+                    where PRICE.LOTTERY_DRAW_ID = p_lottery_draw_id
+                      and HITS_COUNT = p_hits_count)
+            into p_price_won
+            from dual;
+
+            if p_price_won IS NOT NULL then
+                update ENTRY
+                set PRICE_WON = p_price_won
+                where ID = p_entry.ID;
+            else
+                update ENTRY
+                set PRICE_WON = 0
+                where ID = p_entry.ID;
+            end if;
+        end loop;
     commit;
 end;
 /
