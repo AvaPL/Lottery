ea0f4fce8e48e8ca822abb3a56e0f3bef1551a37
agngan <ganowicz.agnieszka@gmail.com>
2020-04-25 15:49:09 +0200

    Moved state from BuyACouponTable to BuyACoupon.

--- frontend/src/components/BuyACoupon/BuyACouponTable.js
+++ frontend/src/components/BuyACoupon/BuyACouponTable.js
@@ -5,39 +5,35 @@ import Button from "react-bootstrap/Button";
 import "./BuyACouponTable.css"
 
 class BuyACouponTable extends Component {
-    state = {
-        entries: [
-            {id: 1, type: "lotto", numbers: "1 8 15"},
-            {id: 2, type: "mini lotto", numbers: "1 3 4"},
-            {id: 3, type: "multi multi", numbers: "12 15 21 57 13 19"},
-            {id: 4, type: "euro jackpot", numbers: "2 15 61"},
-            {id: 5, type: "lotto", numbers: "8 1 3"},
-        ]
-    };
-
-    handleDelete = id => {
-        const entries = this.state.entries.filter(e => e.id !== id);
-        entries.forEach((e, i) => e.id = i + 1);
-        this.setState({entries});
-    };
-
-    handleAdd = () => {
-        const entries = this.state.entries.concat({
-            id: this.state.entries[this.state.entries.length - 1].id + 1,
-            type: "lotto",
-            numbers: "1 2 3"
-        });
-        this.setState({entries});
-    };
+    // state = {
+    //     entries: [
+    //         {id: 1},
+    //         {id: 2},
+    //         {id: 3}
+    //     ]
+    // };
+    //
+    // handleDelete = id => {
+    //     const entries = this.state.entries.filter(e => e.id !== id);
+    //     entries.forEach((e, i) => e.id = i + 1);
+    //     this.setState({entries});
+    // };
+    //
+    // handleAdd = () => {
+    //     const entries = this.state.entries.concat({
+    //         id: this.state.entries[this.state.entries.length - 1].id + 1
+    //     });
+    //     this.setState({entries});
+    // };
 
     render() {
         return (
             <div className="scroll-table overflow-auto scrollbar">
-                {this.state.entries.map(entry => (
-                    <BuyACouponTableEntry key={entry.id} onDelete={this.handleDelete} entry={entry}/>
+                {this.props.entries.map(entry => (
+                    <BuyACouponTableEntry key={entry.id} onDelete={this.props.onDelete} entry={entry}/>
                 ))}
                 <div className="add-button" style={{position: "relative"}}>
-                    <Button className="btn-success btn-block" onClick={this.handleAdd}><span
+                    <Button className="btn-success btn-block" onClick={this.props.onAdd}><span
                         className="add-button-text">+</span></Button>
                 </div>
             </div>
--- frontend/src/pages/BuyACoupon.js
+++ frontend/src/pages/BuyACoupon.js
@@ -5,13 +5,34 @@ import BuyACouponTableHeader from "../components/BuyACoupon/BuyACouponTableHeade
 import BuyACouponTable from "../components/BuyACoupon/BuyACouponTable";
 import "../components/BuyACouponButton/BuyACouponButton.css";
 
-class MyCoupons extends Component {
+class BuyACoupon extends Component {
+    state = {
+        entries: [
+            {id: 1},
+            {id: 2},
+            {id: 3}
+        ]
+    };
+
+    handleDelete = id => {
+        const entries = this.state.entries.filter(e => e.id !== id);
+        entries.forEach((e, i) => e.id = i + 1);
+        this.setState({entries});
+    };
+
+    handleAdd = () => {
+        const entries = this.state.entries.concat({
+            id: this.state.entries[this.state.entries.length - 1].id + 1
+        });
+        this.setState({entries});
+    };
+
     render() {
         return (
             <div className="BuyACoupon">
                 <Header text="BUY A COUPON"/>
                 <BuyACouponTableHeader/>
-                <BuyACouponTable/>
+                <BuyACouponTable entries={this.state.entries} onAdd={this.handleAdd} onDelete={this.handleDelete} />
                 <p><a href={"/my-coupons"} className="btn btn-buy mt-3"><span
                     className="btn-buy-text">BUY</span></a></p>
             </div>
@@ -19,4 +40,4 @@ class MyCoupons extends Component {
     }
 }
 
-export default MyCoupons;
+export default BuyACoupon;
