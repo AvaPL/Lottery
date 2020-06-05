904297073a3ac855884804e73548cf7527d03562
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-06-05 20:51:33 +0200

    Minor code cleanup.

--- backend/src/test/java/com/lottery/email/EmailServiceTest.java
+++ backend/src/test/java/com/lottery/email/EmailServiceTest.java
@@ -4,7 +4,6 @@ import com.lottery.converters.NumberConverter;
 import com.lottery.entities.DrawType;
 import com.lottery.entities.LotteryDraw;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mail.MailException;
@@ -21,6 +20,50 @@ import static org.junit.Assert.*;
 
 class EmailServiceTest {
 
+    private JavaMailSenderMock mockMailSender;
+    private EmailService emailService;
+
+    @BeforeEach
+    void setUp() {
+        mockMailSender = new JavaMailSenderMock();
+        emailService = new EmailService(mockMailSender);
+    }
+
+    @Test
+    void shouldSendCorrectMessage() {
+        String[] recipients = {"andrzej@gmail.com"};
+        LotteryDraw lotteryDraw = createLotteryDraw();
+        emailService.sendLotteryPerformedNotifications(recipients, lotteryDraw);
+        SimpleMailMessage messageSent = mockMailSender.getSentMessage();
+
+        assertEquals("Lottery Team <noreply@lottery.com>", messageSent.getFrom());
+        assertArrayEquals(recipients, messageSent.getTo());
+        assertEquals("Lottery - check if you are the lucky winner!", messageSent.getSubject());
+        assertNotNull(messageSent.getText());
+        assertTrue(messageSent.getText().contains(
+                "We'd like to inform you that the lottery " + lotteryDraw.getDrawType().getName()));
+        assertTrue(messageSent.getText().contains("has reached its end on " + lotteryDraw.getDrawTime()));
+        assertTrue(messageSent.getText().contains("The numbers " + getNumbersString(lotteryDraw) + " were drawn."));
+    }
+
+    private LotteryDraw createLotteryDraw() {
+        LotteryDraw lotteryDraw = new LotteryDraw();
+        lotteryDraw.setDrawTime(LocalDate.of(2020, 7, 1));
+        lotteryDraw.setNumbers(12345L);
+        lotteryDraw.setDrawType(createDrawType());
+        return lotteryDraw;
+    }
+
+    private DrawType createDrawType() {
+        DrawType drawType = new DrawType();
+        drawType.setName("Lotto");
+        return drawType;
+    }
+
+    private String getNumbersString(LotteryDraw lotteryDraw) {
+        return Arrays.toString(NumberConverter.toArray(lotteryDraw.getNumbers()));
+    }
+
     private static class JavaMailSenderMock implements JavaMailSender {
         private SimpleMailMessage message;
 
@@ -33,7 +76,7 @@ class EmailServiceTest {
         public void send(@NotNull SimpleMailMessage... simpleMailMessages) throws MailException {
         }
 
-        public SimpleMailMessage getSentMessage(){
+        public SimpleMailMessage getSentMessage() {
             return message;
         }
 
@@ -68,51 +111,5 @@ class EmailServiceTest {
         public void send(@NotNull MimeMessagePreparator... mimeMessagePreparators) throws MailException {
             throw new UnsupportedOperationException();
         }
-    }
-
-    private JavaMailSenderMock mockMailSender;
-    private EmailService emailService;
-
-    @BeforeEach
-    void setUp() {
-        mockMailSender = new JavaMailSenderMock();
-        emailService = new EmailService(mockMailSender);
-    }
-
-    @Test
-    void shouldSendCorrectMessage() {
-        String[] recipients = {"andrzej@gmail.com"};
-        LotteryDraw lotteryDraw = getLotteryDraw();
-        emailService.sendLotteryPerformedNotifications(recipients, lotteryDraw);
-        SimpleMailMessage messageSent = mockMailSender.getSentMessage();
-
-        assertEquals("Lottery Team <noreply@lottery.com>", messageSent.getFrom());
-        assertArrayEquals(recipients, messageSent.getTo());
-        assertEquals("Lottery - check if you are the lucky winner!", messageSent.getSubject());
-        assertNotNull(messageSent.getText());
-        assertTrue(messageSent.getText().contains("We'd like to inform you that the lottery " + lotteryDraw.getDrawType().getName()));
-        assertTrue(messageSent.getText().contains("has reached its end on " + lotteryDraw.getDrawTime()));
-        assertTrue(messageSent.getText().contains("The numbers " + getStringNumbers(lotteryDraw) + " were drawn."));
-    }
-
-    @NotNull
-    private String getStringNumbers(LotteryDraw lotteryDraw) {
-        return Arrays.toString(NumberConverter.toArray(lotteryDraw.getNumbers()));
-    }
-
-    @NotNull
-    private LotteryDraw getLotteryDraw() {
-        LotteryDraw lotteryDraw = new LotteryDraw();
-        lotteryDraw.setDrawTime(LocalDate.of(2020,7,1));
-        lotteryDraw.setNumbers(12345L);
-        lotteryDraw.setDrawType(getDrawType());
-        return lotteryDraw;
-    }
-
-    @NotNull
-    private DrawType getDrawType() {
-        DrawType drawType = new DrawType();
-        drawType.setName("Lotto");
-        return drawType;
     }
 }
